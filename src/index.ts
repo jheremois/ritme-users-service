9f91d9f58a256aa5b17a12c114c4e999a1ec8f11
jheremy <jheremy808@gmail.com>
2021-12-29 16:53:20 -0400

    fix req max size

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,8 @@ const server = http.createServer(app);
 dotenv.config({path: './.env'})
 app.set('port', process.env.PORT || 4100)
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit: '5mb'}));
+app.use(express.urlencoded({limit: '5mb'}));
 
 const port = app.get('port')
 

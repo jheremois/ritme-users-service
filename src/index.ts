f0ef5c3af27de071a5419d077ef8e71233131323
jheremy <jheremy802@gmail.com>
2022-12-04 19:07:11 -0400

    add cors origin

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,8 @@ const server = http.createServer(app);
 dotenv.config({path: './.env'})
 app.set('port', process.env.PORT || 4100)
 
-const corsOptions = {
-	origin: appConfig.cors.allowOrigin,
-	optionsSuccessStatus: 200
-};
+app.use(cors())
 
-app.use(cors(corsOptions))
 app.use(express.json({limit: '15mb'}));
 
 const port = app.get('port')
